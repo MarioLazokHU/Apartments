2f0cba0cef6ba8a695b3e636bb4e5bd8bb457cbb
MarioLazokHU <lazokmarios7@gmail.com>
2023-08-04 18:41:48 +0200

    feat: add redux provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import './input.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './services/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = document.getElementById('root');
+const rootComponent = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootElement = createRoot(root);
+rootElement.render(rootComponent);
+
 reportWebVitals();
